93718898b574f456cd2183f4d77dbf6a7a8396cc
zhanglei <zhanglei@guoshengtianfeng.com>
2018-01-13 19:25:26 +0800

    init

---
+++ src/main/java/com/wangxiaobao/wechatgateway/WechatGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.wangxiaobao.wechatgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatGatewayApplication.class, args);
+	}
+}
