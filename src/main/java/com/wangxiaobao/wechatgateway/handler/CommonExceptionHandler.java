6b6605db6486c92567f2179eab85a9763a9ac4e9
liping <liping@guoshengtianfeng.com>
2018-01-30 17:08:22 +0800

    打印错误日志

--- src/main/java/com/wangxiaobao/wechatgateway/handler/CommonExceptionHandler.java
+++ src/main/java/com/wangxiaobao/wechatgateway/handler/CommonExceptionHandler.java
@@ -1,15 +1,15 @@
 package com.wangxiaobao.wechatgateway.handler;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.wangxiaobao.wechatgateway.VO.ResultVO;
 import com.wangxiaobao.wechatgateway.enums.ResultEnum;
 import com.wangxiaobao.wechatgateway.exception.CommonException;
 import com.wangxiaobao.wechatgateway.utils.ResultVOUtil;
+
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * Created by halleyzhang on 2018/1/14.
@@ -20,14 +20,14 @@ public class CommonExceptionHandler {
   @ExceptionHandler(CommonException.class)
   @ResponseBody
   public ResultVO handlerCommonException(CommonException e) {
-    log.error("【通用异常】",e.getMessage());
+    log.error("【通用异常】",e);
     return ResultVOUtil.error(e.getCode() , e.getMessage());
   }
 
   @ExceptionHandler(Exception.class)
   @ResponseBody
   public ResultVO handlerException(Exception e) {
-    log.error("【未捕获异常】",e.getMessage());
+    log.error("【未捕获异常】{}",e);
     return ResultVOUtil.error(ResultEnum.UNKNOW_ERROR.getCode(), e.getMessage());
   }
 }
