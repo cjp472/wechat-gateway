7e985fc7b291d4baf14ae4d7c4cdad5f42d8c2e5
liping <liping@guoshengtianfeng.com>
2018-02-09 14:14:15 +0800

    去掉多余的引入

--- src/main/java/com/wangxiaobao/wechatgateway/WechatGatewayApplication.java
+++ src/main/java/com/wangxiaobao/wechatgateway/WechatGatewayApplication.java
@@ -2,25 +2,20 @@ package com.wangxiaobao.wechatgateway;
 
 import java.util.List;
 
-import com.wangxiaobao.wechatgateway.utils.LoginUserInfoMethodArgumentResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import com.source3g.springboot.frame.annotations.WxbRestTemplate;
+import com.wangxiaobao.wechatgateway.utils.LoginUserInfoMethodArgumentResolver;
 import com.wangxiaobao.wechatgateway.utils.UserInfoMethodArgumentResolver;
 @SpringBootApplication
+@WxbRestTemplate
 public class WechatGatewayApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WechatGatewayApplication.class, args);
-	}
-	
-	@Bean
-	public RestTemplate getRestTemplate(){
-		return new RestTemplate();
 	}
 
 	@Override
