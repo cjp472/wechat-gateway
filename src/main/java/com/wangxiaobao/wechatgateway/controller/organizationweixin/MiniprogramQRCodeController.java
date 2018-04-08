8cc20049de56eb8b87a0b988b8450b1e8d81455f
liping <liping@guoshengtianfeng.com>
2018-04-08 10:39:28 +0800

    整理方法的位置

--- src/main/java/com/wangxiaobao/wechatgateway/controller/organizationweixin/MiniprogramQRCodeController.java
+++ src/main/java/com/wangxiaobao/wechatgateway/controller/organizationweixin/MiniprogramQRCodeController.java
@@ -78,29 +78,6 @@ public class MiniprogramQRCodeController extends BaseController {
 		BeanUtils.copyProperties(request, miniprogramQrCodeAddForm);
 		return JsonResult.newInstanceDataSuccess(miniProgramQrCodeService.qrcodejumpadd(miniprogramQrCodeAddForm, request.getWxAppid()));
 	}
-	/**
-	  * @methodName: qrcodejumpadd
-	  * @Description: TODO完整设置发布二维码规则
-	  * @param request
-	  * @param bindingResult
-	  * @return JsonResult
-	  * @createUser: liping_max
-	  * @createDate: 2018年1月25日 下午6:41:21
-	  * @updateUser: liping_max
-	  * @updateDate: 2018年1月25日 下午6:41:21
-	  * @throws
-	 */
-	@RequestMapping("/miniprogramqrcode/qrcodejumpaddAndPublish")
-	public JsonResult qrcodejumpaddAndPublish(@Valid MiniprogramQrCodeAddRequest request, BindingResult bindingResult){
-		if (bindingResult.hasErrors()) {
-			log.error("【增加或修改二维码规则】参数不正确, MiniprogramQrCodeAddRequest={}", request);
-			throw new CommonException(ResultEnum.PARAM_ERROR.getCode(),
-					bindingResult.getFieldError().getDefaultMessage());
-		}
-		MiniprogramQrCodeAddForm miniprogramQrCodeAddForm = new MiniprogramQrCodeAddForm();
-		BeanUtils.copyProperties(request, miniprogramQrCodeAddForm);
-		return JsonResult.newInstanceDataSuccess(miniProgramQrCodeService.qrcodejumpaddAndPush(miniprogramQrCodeAddForm, request.getWxAppid()));
-	}
 	
 	/**
 	  * @methodName: qrcodejumpdelete
--- src/main/java/com/wangxiaobao/wechatgateway/controller/organizationweixin/QrcodeUrlVerifyController.java
+++ src/main/java/com/wangxiaobao/wechatgateway/controller/organizationweixin/QrcodeUrlVerifyController.java
@@ -2,15 +2,21 @@ package com.wangxiaobao.wechatgateway.controller.organizationweixin;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wangxiaobao.wechatgateway.controller.base.BaseController;
 import com.wangxiaobao.wechatgateway.entity.qrcodeurlverify.QrcodeUrlVerify;
-import com.wangxiaobao.wechatgateway.form.miniprogramqrcode.MiniprogramQrCodeRequest;
+import com.wangxiaobao.wechatgateway.enums.ResultEnum;
+import com.wangxiaobao.wechatgateway.exception.CommonException;
+import com.wangxiaobao.wechatgateway.form.miniprogramqrcode.MiniprogramQrCodeAddForm;
+import com.wangxiaobao.wechatgateway.form.miniprogramqrcode.MiniprogramQrCodeAddRequest;
+import com.wangxiaobao.wechatgateway.service.miniprogramqrcode.MiniProgramQrCodeService;
 import com.wangxiaobao.wechatgateway.service.qrcodeurlverify.QrcodeUrlVerifyService;
 import com.wangxiaobao.wechatgateway.utils.JsonResult;
 
@@ -20,10 +26,31 @@ import lombok.extern.slf4j.Slf4j;
 public class QrcodeUrlVerifyController extends BaseController {
 	@Autowired
 	private QrcodeUrlVerifyService qrcodeUrlVerifyService;
+	@Autowired
+	private MiniProgramQrCodeService miniProgramQrCodeService;
 	
-	public JsonResult qrcodejumpdownload(@Valid MiniprogramQrCodeRequest request){
-		
-		return null;
+	/**
+	  * @methodName: qrcodejumpadd
+	  * @Description: TODO完整设置发布二维码规则
+	  * @param request
+	  * @param bindingResult
+	  * @return JsonResult
+	  * @createUser: liping_max
+	  * @createDate: 2018年1月25日 下午6:41:21
+	  * @updateUser: liping_max
+	  * @updateDate: 2018年1月25日 下午6:41:21
+	  * @throws
+	 */
+	@RequestMapping("/miniprogramqrcode/qrcodejumpaddAndPublish")
+	public JsonResult qrcodejumpaddAndPublish(@Valid MiniprogramQrCodeAddRequest request, BindingResult bindingResult){
+		if (bindingResult.hasErrors()) {
+			log.error("【增加或修改二维码规则】参数不正确, MiniprogramQrCodeAddRequest={}", request);
+			throw new CommonException(ResultEnum.PARAM_ERROR.getCode(),
+					bindingResult.getFieldError().getDefaultMessage());
+		}
+		MiniprogramQrCodeAddForm miniprogramQrCodeAddForm = new MiniprogramQrCodeAddForm();
+		BeanUtils.copyProperties(request, miniprogramQrCodeAddForm);
+		return JsonResult.newInstanceDataSuccess(miniProgramQrCodeService.qrcodejumpaddAndPush(miniprogramQrCodeAddForm, request.getWxAppid()));
 	}
 	
 	/**
